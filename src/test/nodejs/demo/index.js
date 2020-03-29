11c62f3eef635c502fc37314cecfb9db0e81c6b6
linux_china <libing.chen@gmail.com>
2020-03-29 16:47:28 -0700

    polishing

--- src/test/nodejs/demo/index.js
+++ src/test/nodejs/demo/index.js
@@ -1,6 +1,7 @@
-const userService = require("./UserController");
+//const userService = require("./UserController");
+const userController = require("@UserService/UserController").setBaseUrl("http://localhost:8080");
 
 (async () => {
-    let nick = await userService.findNickById(1);
+    let nick = await userController.findNickById(1);
     console.log(nick);
 })()
