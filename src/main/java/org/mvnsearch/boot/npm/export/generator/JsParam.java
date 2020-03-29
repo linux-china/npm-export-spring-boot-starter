ba89e743ff41a5393d67b71f8ccf706d4b4c3d06
linux_china <libing.chen@gmail.com>
2020-03-28 22:20:26 -0700

    js method param

---
+++ src/main/java/org/mvnsearch/boot/npm/export/generator/JsParam.java
@@ -0,0 +1,82 @@
+package org.mvnsearch.boot.npm.export.generator;
+
+/**
+ * js method param
+ *
+ * @author linux_china
+ */
+public class JsParam implements JavaToJsTypeConverter {
+    private String name;
+    private Class<?> type;
+    private String pathVariableName;
+    private String requestParamName;
+    private String httpHeaderName;
+    private boolean bodyData;
+
+    public JsParam() {
+    }
+
+    public JsParam(String name, Class<?> type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Class<?> getType() {
+        return type;
+    }
+
+    public void setType(Class<?> type) {
+        this.type = type;
+    }
+
+    public String getJsType() {
+        return toJsType(this.type);
+    }
+
+    public String getPathVariableName() {
+        return pathVariableName;
+    }
+
+    public void setPathVariableName(String pathVariableName) {
+        this.pathVariableName = pathVariableName;
+    }
+
+    public String getRequestParamName() {
+        return requestParamName;
+    }
+
+    public void setRequestParamName(String requestParamName) {
+        this.requestParamName = requestParamName;
+    }
+
+    public String getHttpHeaderName() {
+        return httpHeaderName;
+    }
+
+    public void setHttpHeaderName(String httpHeaderName) {
+        this.httpHeaderName = httpHeaderName;
+    }
+
+    public boolean isBodyData() {
+        return bodyData;
+    }
+
+    public void setBodyData(boolean bodyData) {
+        this.bodyData = bodyData;
+    }
+
+    public boolean isFromRequestSide() {
+        return this.pathVariableName != null
+                || this.requestParamName != null
+                || this.httpHeaderName != null
+                || this.bodyData;
+    }
+}
