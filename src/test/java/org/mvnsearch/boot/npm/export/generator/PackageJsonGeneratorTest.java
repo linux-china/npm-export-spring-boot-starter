e2654287a7bf955211d16fca56ccc0dd7498eacb
linux_china <libing.chen@gmail.com>
2020-03-29 11:10:20 -0700

    polishing

--- src/test/java/org/mvnsearch/boot/npm/export/generator/PackageJsonGeneratorTest.java
+++ src/test/java/org/mvnsearch/boot/npm/export/generator/PackageJsonGeneratorTest.java
@@ -8,11 +8,11 @@ import org.junit.jupiter.api.Test;
  * @author linux_china
  */
 public class PackageJsonGeneratorTest {
-    private final PackageJsonGenerator generator = new PackageJsonGenerator();
+    private final PackageJsonGenerator generator = new PackageJsonGenerator("@UserService/UserController", "2020.3.2");
 
     @Test
     public void testGenerate() throws Exception {
-        generator.addContext("packageName", "@UserService/UserController");
+        generator.addContext("description", "package to call REST API");
         System.out.println(generator.generate());
     }
 }
