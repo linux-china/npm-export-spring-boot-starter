8c041bc9422e2020bcf988f5de143373ce71f233
linux_china <libing.chen@gmail.com>
2020-03-28 23:07:00 -0700

    add toTypeDef

--- src/main/java/org/mvnsearch/boot/npm/export/generator/JavaToJsTypeConverter.java
+++ src/main/java/org/mvnsearch/boot/npm/export/generator/JavaToJsTypeConverter.java
@@ -1,21 +1,49 @@
 package org.mvnsearch.boot.npm.export.generator;
 
+import java.lang.reflect.Field;
+import java.util.*;
+
 /**
  * java to javascript type converter
  *
  * @author linux_china
  */
 public interface JavaToJsTypeConverter {
+    static List<Class<?>> numberClazzList = Arrays.asList(Integer.class, int.class,
+            Long.class, long.class,
+            Float.class, float.class,
+            Double.class, double.class,
+            Byte.class, byte.class);
+
     default String toJsType(Class<?> type) {
-        if (type.isAssignableFrom(String.class)) {
+        if (type.isAssignableFrom(String.class)
+                || type.isAssignableFrom(Date.class)
+                || type.getCanonicalName().startsWith("java.time.")
+                || type.isAssignableFrom(UUID.class)) {
             return "string";
         } else if (type.isAssignableFrom(Boolean.class) || type.isAssignableFrom(boolean.class)) {
             return "bool";
-        } else if (type.isAssignableFrom(Integer.class)
-                || type.isAssignableFrom(int.class)) {
+        } else if (numberClazzList.contains(type)) {
             return "number";
-        } else {
+        } else if (type.isAssignableFrom(List.class)
+                || type.isAssignableFrom(Set.class)
+                || type.isArray()) {
+            return "Array";
+        } else if (type.isAssignableFrom(Map.class)) {
             return "Object";
+        } else {
+            return type.getSimpleName();
         }
+    }
+
+    default String toTypeDef(Class<?> clazz) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("/**\n");
+        builder.append(" * @typedef {Object} " + clazz.getSimpleName() + "\n");
+        for (Field field : clazz.getDeclaredFields()) {
+            builder.append(" * @property {" + toJsType(field.getType()) + "} " + field.getName() + "\n");
+        }
+        builder.append(" */\n");
+        return builder.toString();
     }
 }
