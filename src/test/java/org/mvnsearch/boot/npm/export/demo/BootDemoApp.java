0ee4debd33de90c6e0f9ed6a419b17c0e3aeb557
linux_china <libing.chen@gmail.com>
2020-03-28 23:11:24 -0700

    boot demo app

---
+++ src/test/java/org/mvnsearch/boot/npm/export/demo/BootDemoApp.java
@@ -0,0 +1,16 @@
+package org.mvnsearch.boot.npm.export.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * boot demo app
+ *
+ * @author linux_china
+ */
+@SpringBootApplication
+public class BootDemoApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BootDemoApp.class, args);
+    }
+}
