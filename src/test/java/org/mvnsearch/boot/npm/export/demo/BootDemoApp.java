0c0e3a1ac2281259db151e4fd4054863b974ff4b
linux_china <libing.chen@gmail.com>
2020-03-30 14:27:51 -0700

    add @OpenAPIDefinition

--- src/test/java/org/mvnsearch/boot/npm/export/demo/BootDemoApp.java
+++ src/test/java/org/mvnsearch/boot/npm/export/demo/BootDemoApp.java
@@ -1,5 +1,7 @@
 package org.mvnsearch.boot.npm.export.demo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author linux_china
  */
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "UserService App", description = "User related REST API"))
 public class BootDemoApp {
     public static void main(String[] args) {
         SpringApplication.run(BootDemoApp.class, args);
