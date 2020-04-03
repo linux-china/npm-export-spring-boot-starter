31d0f40e1ac9687307b966b0b08b4f28ffc4800d
linux_china <libing.chen@gmail.com>
2020-04-03 12:11:27 -0700

    polishing

--- src/main/java/org/mvnsearch/boot/npm/export/NpmPackage.java
+++ src/main/java/org/mvnsearch/boot/npm/export/NpmPackage.java
@@ -1,13 +1,18 @@
 package org.mvnsearch.boot.npm.export;
 
+import java.lang.annotation.*;
+
 /**
  * npm package annotation
  *
  * @author linux_china
  */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface NpmPackage {
 
-    String name() default "";
+    String value() default "";
 
-    String version() default "1.0.0";
+    String version() default "";
 }
