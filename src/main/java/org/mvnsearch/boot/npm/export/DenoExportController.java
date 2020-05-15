ec376fab030b1c2a15fbacd4251e5c7c187400bc
linux_china <libing.chen@gmail.com>
2020-05-15 14:37:04 -0700

    Deno export Controller

---
+++ src/main/java/org/mvnsearch/boot/npm/export/DenoExportController.java
@@ -0,0 +1,34 @@
+package org.mvnsearch.boot.npm.export;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.server.ServerWebExchange;
+
+import java.io.IOException;
+
+/**
+ * Deno export Controller
+ *
+ * @author linux_china
+ */
+@Controller
+public class DenoExportController {
+    @Autowired
+    private ApplicationContext applicationContext;
+    @Autowired
+    private Environment env;
+
+    @GetMapping(value = "/deno/{packageName}/mod.ts", produces = {"text/x.typescript"})
+    @ResponseBody
+    public String npmPackage(@PathVariable("packageName") String packageName, ServerWebExchange exchange) throws IOException {
+        String uri = exchange.getRequest().getURI().toString();
+        String baseUrl = uri.substring(0, uri.indexOf("/", 9));
+        return "export function hello() {}";
+    }
+
+}
